8c5b55357f4570c75017bf2e77be1dbe38c08a86
Deight <d8corp@mail.ru>
2026-07-16 16:57:02 +0300

    First implementation

---
+++ src/hooks/persistent/persistent.test.ts
@@ -0,0 +1,105 @@
+import { get, Hub, set } from 'rune-hub'
+
+import { persistent } from './persistent'
+
+afterEach(() => {
+  localStorage.clear()
+})
+
+describe('persistent', () => {
+  describe('Only key', () => {
+    it('Should return null by default', () => {
+      const hub = new Hub()
+      const state = () => persistent('state')
+
+      hub.use(() => {
+        expect(get(state)).toBe(null)
+      })
+    })
+
+    it('Can be cleared by null', () => {
+      const hub = new Hub()
+      const state = () => persistent('state')
+
+      hub.use(() => {
+        set(state, '')
+        expect(get(state)).toBe('')
+        expect(localStorage.getItem('state')).toBe('')
+
+        set(state, null)
+        expect(get(state)).toBe(null)
+        expect(localStorage.getItem('state')).toBe(null)
+        expect(localStorage.length).toBe(0)
+      })
+    })
+  })
+
+  it('Should return initial value', () => {
+    const hub = new Hub()
+    const state = () => persistent('state', '')
+
+    hub.use(() => {
+      expect(get(state)).toBe('')
+    })
+  })
+
+  it('Should return initial value for multiplay states', () => {
+    const hub = new Hub()
+    const state1 = () => persistent('state', '')
+    const state2 = () => persistent('state', null)
+
+    hub.use(() => {
+      expect(get(state1)).toBe('')
+      expect(get(state2)).toBe(null)
+    })
+  })
+
+  it('Should change the storage', () => {
+    const hub = new Hub()
+    const state = () => persistent('state', '')
+
+    hub.use(() => {
+      set(state, 'foo')
+
+      expect(get(state)).toBe('foo')
+    })
+
+    expect(localStorage.getItem('state')).toBe('foo')
+  })
+
+  it('Should init from storage', () => {
+    const hub = new Hub()
+    const state = () => persistent('state', '')
+
+    localStorage.setItem('state', 'foo')
+
+    hub.use(() => {
+      expect(get(state)).toBe('foo')
+    })
+  })
+
+  it('Should keep set initial value in storage', () => {
+    const hub = new Hub()
+    const state = () => persistent('state', '')
+
+    hub.use(() => {
+      set(state, 'foo')
+      set(state, '')
+      expect(get(state)).toBe('')
+    })
+
+    expect(localStorage.getItem('state')).toBe('')
+  })
+
+  it('Should not keep unchanged initial value in storage', () => {
+    const hub = new Hub()
+    const state = () => persistent('state', '')
+
+    hub.use(() => {
+      set(state, '')
+      expect(get(state)).toBe('')
+    })
+
+    expect(localStorage.getItem('state')).toBe(null)
+  })
+})
---
+++ src/hooks/persistent/persistent.ts
@@ -0,0 +1,83 @@
+import { Hub, type Slot } from 'rune-hub'
+
+export type PersistentStorage = Record<string, string | null>
+export type PersistentDecode<T> = (v: string) => T
+export type PersistentEncode<T> = (v: T) => string | null
+
+export interface PersistentParams<T> {
+  decode: PersistentDecode<T>
+  encode: PersistentEncode<T>
+  storage?: PersistentStorage
+  listen?: boolean
+}
+
+const asIs = <T> (v: T): T => v
+
+export function persistent (key: string): string | null
+export function persistent<T, I> (key: string, init: I, opt: PersistentParams<T | I>): I | (unknown extends T ? string : T)
+export function persistent<T, I extends string | null> (key: string, init: I, opt?: Partial<PersistentParams<T | I>>): I | (unknown extends T ? string : T)
+
+export function persistent (key: string, init: any = null, opt?: Partial<PersistentParams<any>>) {
+  const ctx = (Hub.cur ?? Hub.root).ctx as Slot
+  if (!ctx) return init
+
+  const storage = opt?.storage ?? (typeof localStorage !== 'undefined' ? localStorage : undefined)
+  if (!storage) return init
+
+  const encode = (opt?.encode ?? asIs)
+  const decode = (opt?.decode ?? asIs)
+
+  const initDecode = (value: string | null = null) => {
+    return value === null ? init : decode(value)
+  }
+
+  const setFromStorage = (value: string | null) => {
+    ctx.set(initDecode(value))
+  }
+
+  if (!ctx.inited) {
+    ctx.on('change', () => {
+      const value = encode(ctx.cur)
+
+      if (value === null) {
+        delete storage[key]
+      } else {
+        storage[key] = value
+      }
+    })
+
+    ctx.on('get', () => {
+      if (!ctx.up) {
+        const cur = initDecode(storage[key])
+
+        if (ctx.cur !== cur) {
+          ctx.prev = ctx.cur
+          ctx.cur = cur
+        }
+      }
+    })
+
+    if (typeof window !== 'undefined' && opt?.listen !== false) {
+      const listener = (e: StorageEvent) => {
+        if (e.key !== key) return
+        setFromStorage(e.newValue)
+      }
+
+      const restore = () => {
+        setFromStorage(storage[key])
+      }
+
+      ctx.on('up', () => {
+        window.addEventListener('storage', listener)
+        window.addEventListener('pageshow', restore)
+      })
+
+      ctx.on('down', () => {
+        window.removeEventListener('storage', listener)
+        window.removeEventListener('pageshow', restore)
+      })
+    }
+  }
+
+  return initDecode(storage[key])
+}
